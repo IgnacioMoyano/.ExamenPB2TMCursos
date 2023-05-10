3867def329f8624fe5b087163c3585623f77868e
“Ignacio <nachorf78@gmail.com>
2023-05-10 10:54:53 -0300

    Estructura Inicial Persona

--- src/ar/edu/unlam/dominio/EmpresaDeCursos/Persona.java
+++ src/ar/edu/unlam/dominio/EmpresaDeCursos/Persona.java
@@ -2,4 +2,33 @@ package ar.edu.unlam.dominio.EmpresaDeCursos;
 
 public class Persona {
 
+	    private String nombre;
+	    private String apellido;
+	    private Integer dni;
+	    private String email;
+	    private String telefono;
+
+
+
+
+
+	public Persona() {
+
+	}
+
+
+
+	public Persona(String nombre, String apellido, Integer dni, String email, String telefono) {
+	    this.apellido = apellido;
+	    this.dni = dni;
+	    this.email = email;
+	    this.telefono = telefono;
+	}
+	
+	
+	
+	
+	
 }
+
+ 
