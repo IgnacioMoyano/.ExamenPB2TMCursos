1663a3b1c07592bd87e862a2440c88b5e07e1dd0
“Lucas <lucashornos04@gmail.com>
2023-05-10 14:20:21 -0300

    TestCursosYClaseCurso

--- src/ar/edu/unlam/dominio/EmpresaDeCursos/Curso.java
+++ src/ar/edu/unlam/dominio/EmpresaDeCursos/Curso.java
@@ -1,5 +1,142 @@
 package ar.edu.unlam.dominio.EmpresaDeCursos;
 
+import java.util.ArrayList;
+
 public class Curso {
+
+	private final Integer EDAD_MINIMA_HTML = 15;
+	private final Integer EDAD_MAXIMA_HTML = 30;
+
+	private final Integer EDAD_MINIMA_CSS = 18;
+	private final Integer EDAD_MAXIMA_CSS = 35;
+
+	private final Integer EDAD_MINIMA_JAVA = 20;
+	private final Integer EDAD_MAXIMA_JAVA = 40;
+
+	private ArrayList<Alumno> alumnos;
+	private Profesor profesor;
+	private TipoCurso tipoCurso;
+	private Integer duracion;
+	private Integer precio;
+	private String codigoCurso;
+
+	public Curso(TipoCurso tipoCurso) {
+		this.alumnos = new ArrayList<>();
+		this.tipoCurso = tipoCurso;
+	}
+
+	public void añadirProfesor(Profesor profesor) {
+
+		if (profesor.getNombre() != null && profesor.getApellido() != null && profesor.getDni() != null
+				&& profesor.getEmail() != null && profesor.getTelefeno() != null) {
+			setProfesor(profesor);
+		}
+
+	}
+
+	public void añadirAlumnos(Alumno alumno, TipoCurso tipoCurso) {
+		Integer capacidadMaximaPersonas = 10;
+
+		if (alumno.getNombre() != null && alumno.getApellido() != null && alumno.getDni() != null
+				&& alumno.getEmail() != null && alumno.getTelefeno() != null) {
+			if (edadRequeridaParaCursar(alumno, tipoCurso)) {
+				if (alumnos.size() < capacidadMaximaPersonas) {
+					alumnos.add(alumno);
+				}
+			}
+		}
+	}
+
+	public Integer cantidadAlumnos() {
+		return alumnos.size();
+	}
+
+	public void duracionSegunTipoCurso() {
+		Integer duracionHtml = 100;
+		Integer duracionCss = 150;
+		Integer duracionJava = 200;
+
+		if (getTipoCurso() == TipoCurso.HTML) {
+			setDuracion(duracionHtml);
+		}
+
+		if (getTipoCurso() == TipoCurso.CSS) {
+			setDuracion(duracionCss);
+		}
+
+		if (getTipoCurso() == TipoCurso.JAVA) {
+			setDuracion(duracionJava);
+		}
+
+	}
+
+	public boolean edadRequeridaParaCursar(Alumno alumno, TipoCurso tipoCurso) {
+
+		boolean validado = false;
+		TipoCurso tipoCursoParam = tipoCurso;
+
+		if (tipoCurso == TipoCurso.HTML && alumno.getEdad() > EDAD_MINIMA_HTML && alumno.getEdad() < EDAD_MAXIMA_HTML) {
+			validado = true;
+		}
+
+		if (tipoCurso == TipoCurso.CSS && alumno.getEdad() > EDAD_MINIMA_CSS && alumno.getEdad() < EDAD_MAXIMA_CSS) {
+			validado = true;
+		}
+
+		if (tipoCurso == TipoCurso.JAVA && alumno.getEdad() > EDAD_MINIMA_JAVA && alumno.getEdad() < EDAD_MAXIMA_JAVA) {
+			validado = true;
+		}
+
+		return validado;
+
+	}
+
+	public ArrayList<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(ArrayList<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+
+	public Profesor getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
+	}
+
+	public TipoCurso getTipoCurso() {
+		return tipoCurso;
+	}
+
+	public void setTipoCurso(TipoCurso tipoCurso) {
+		this.tipoCurso = tipoCurso;
+	}
+
+	public Integer getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
+	}
+
+	public Integer getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Integer precio) {
+		this.precio = precio;
+	}
+
+	public String getCodigoCurso() {
+		return codigoCurso;
+	}
+
+	public void setCodigoCurso(String codigoCurso) {
+		this.codigoCurso = codigoCurso;
+	}
 
 }
--- test/ar/edu/unlam/dominio/EmpresaDeCursos/TestCurso.java
+++ test/ar/edu/unlam/dominio/EmpresaDeCursos/TestCurso.java
@@ -2,13 +2,347 @@ package ar.edu.unlam.dominio.EmpresaDeCursos;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class TestCurso {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queSePuedaCrearUnCurso() {
+		final TipoCurso tipoCurso = TipoCurso.HTML;
+		Curso curso;
+
+		curso = new Curso(tipoCurso);
+
+		assertNotNull(curso);
+	}
+
+	@Test
+	public void queSePuedaAñadirUnAlumno() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final Integer cantidadDeAlumnosEsperados;
+		final TipoCurso tipoCurso;
+		final LocalDate fechaNacimiento;
+		Alumno alumno;
+		Curso curso;
+
+		cantidadDeAlumnosEsperados = 1;
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Sebastian";
+		apellido = "Villa";
+		dni = 40402705;
+		email = "ElAlumnoVilla@gmail.com";
+		telefono = "+541128341522";
+		fechaNacimiento = LocalDate.of(1996, 5, 19);
+
+		alumno = new Alumno(nombre, apellido, fechaNacimiento);
+
+		curso = new Curso(tipoCurso);
+
+		alumno.validarYAgregarDni(dni);
+		alumno.validarYAgregarEmail(email);
+		alumno.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirAlumnos(alumno, tipoCurso);
+
+		assertEquals(cantidadDeAlumnosEsperados, curso.cantidadAlumnos());
+	}
+
+	@Test
+	public void queSePuedaAñadirMasDeUnAlumno() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final LocalDate fechaNacimiento;
+
+		Integer cantidadDeAlumnosEsperados;
+		final TipoCurso tipoCurso;
+		Alumno alumno;
+		Curso curso;
+
+		nombre = "Sebastian";
+		apellido = "Villa";
+		dni = 40402722;
+		email = "ElAlumnoVilla@gmail.com";
+		telefono = "+541128341522";
+		fechaNacimiento = LocalDate.of(1996, 5, 19);
+
+		cantidadDeAlumnosEsperados = 4;
+		tipoCurso = TipoCurso.HTML;
+
+		for (int i = 0; i < 4; i++) {
+			alumno = new Alumno(i, i, i, i, i, fechaNacimiento);
+			curso.añadirAlumnos(alumno, tipoCurso);
+		}
+
+		assertEquals(cantidadDeAlumnosEsperados, curso.cantidadAlumnos());
+	}
+
+	@Test
+	public void queNoSePuedaAñadirUnAlumnoSinDniValido() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final Integer cantidadDeAlumnosEsperados;
+		final TipoCurso tipoCurso;
+		final LocalDate fechaNacimiento;
+		Alumno alumno;
+		Curso curso;
+
+		cantidadDeAlumnosEsperados = 0;
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Sebastian";
+		apellido = "Villa";
+		dni = 404027052;
+		email = "ElAlumnoVilla@gmail.com";
+		telefono = "+541128341522";
+		fechaNacimiento = LocalDate.of(1996, 5, 19);
+
+		alumno = new Alumno(nombre, apellido, fechaNacimiento);
+
+		curso = new Curso(tipoCurso);
+
+		alumno.validarYAgregarDni(dni);
+		alumno.validarYAgregarEmail(email);
+		alumno.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirAlumnos(alumno, tipoCurso);
+
+		assertEquals(cantidadDeAlumnosEsperados, curso.cantidadAlumnos());
+	}
+
+	@Test
+	public void queNoSePuedaAñadirUnAlumnoSinEmailValido() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final Integer cantidadDeAlumnosEsperados;
+		final TipoCurso tipoCurso;
+		final LocalDate fechaNacimiento;
+		Alumno alumno;
+		Curso curso;
+
+		cantidadDeAlumnosEsperados = 0;
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Sebastian";
+		apellido = "Villa";
+		dni = 40402705;
+		email = "ElAlumnoVilla%gmail.com";
+		telefono = "+541128341522";
+		fechaNacimiento = LocalDate.of(1996, 5, 19);
+
+		alumno = new Alumno(nombre, apellido, fechaNacimiento);
+
+		curso = new Curso(tipoCurso);
+
+		alumno.validarYAgregarDni(dni);
+		alumno.validarYAgregarEmail(email);
+		alumno.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirAlumnos(alumno, tipoCurso);
+
+		assertEquals(cantidadDeAlumnosEsperados, curso.cantidadAlumnos());
+	}
+
+	@Test
+	public void queNoSePuedaAñadirUnAlumnoSinTelefonoValido() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final Integer cantidadDeAlumnosEsperados;
+		final TipoCurso tipoCurso;
+		final LocalDate fechaNacimiento;
+		Alumno alumno;
+		Curso curso;
+
+		cantidadDeAlumnosEsperados = 0;
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Sebastian";
+		apellido = "Villa";
+		dni = 40402705;
+		email = "ElAlumnoVilla@gmail.com";
+		telefono = "+5411283415222";
+		fechaNacimiento = LocalDate.of(1996, 5, 19);
+
+		alumno = new Alumno(nombre, apellido, fechaNacimiento);
+
+		curso = new Curso(tipoCurso);
+
+		alumno.validarYAgregarDni(dni);
+		alumno.validarYAgregarEmail(email);
+		alumno.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirAlumnos(alumno, tipoCurso);
+
+		assertEquals(cantidadDeAlumnosEsperados, curso.cantidadAlumnos());
+	}
+
+	@Test
+	public void queSePuedaAñadirUnProfesor() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final TipoCurso tipoCurso;
+		Profesor profesor;
+		Curso curso;
+
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Jorge";
+		apellido = "Almiron";
+		dni = 20206295;
+		email = "ElProfeAlmiron@gmail.com";
+		telefono = "+541128341574";
+
+		profesor = new Profesor(nombre, apellido);
+		curso = new Curso(tipoCurso);
+
+		profesor.validarYAgregarDni(dni);
+		profesor.validarYAgregarEmail(email);
+		profesor.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirProfesor(profesor);
+
+		assertEquals(profesor, curso.getProfesor());
+	}
+
+	@Test
+	public void queNoSePuedaAñadirUnProfesorSinDniValido() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final TipoCurso tipoCurso;
+		Profesor profesor;
+		Curso curso;
+
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Jorge";
+		apellido = "Almiron";
+		dni = 202062955;
+		email = "ElProfeAlmiron@gmail.com";
+		telefono = "+541128341574";
+
+		profesor = new Profesor(nombre, apellido);
+		curso = new Curso(tipoCurso);
+
+		profesor.validarYAgregarDni(dni);
+		profesor.validarYAgregarEmail(email);
+		profesor.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirProfesor(profesor);
+
+		assertNull(curso.getProfesor());
+	}
+
+	@Test
+	public void queNoSePuedaAñadirUnProfesorSinEmailValido() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final TipoCurso tipoCurso;
+		Profesor profesor;
+		Curso curso;
+
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Jorge";
+		apellido = "Almiron";
+		dni = 202062955;
+		email = "ElProfeAlmiron%gmail.com";
+		telefono = "+541128341574";
+
+		profesor = new Profesor(nombre, apellido);
+		curso = new Curso(tipoCurso);
+
+		profesor.validarYAgregarDni(dni);
+		profesor.validarYAgregarEmail(email);
+		profesor.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirProfesor(profesor);
+
+		assertNull(curso.getProfesor());
+	}
+
+	@Test
+	public void queNoSePuedaAñadirUnProfesorSinTelefonoValido() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final TipoCurso tipoCurso;
+		Profesor profesor;
+		Curso curso;
+
+		tipoCurso = TipoCurso.HTML;
+		nombre = "Jorge";
+		apellido = "Almiron";
+		dni = 202062955;
+		email = "ElProfeAlmiron@gmail.com";
+		telefono = "+5411283415745";
+
+		profesor = new Profesor(nombre, apellido);
+		curso = new Curso(tipoCurso);
+
+		profesor.validarYAgregarDni(dni);
+		profesor.validarYAgregarEmail(email);
+		profesor.vaidarYAgregarTelefono(telefono);
+
+		curso.añadirProfesor(profesor);
+
+		assertNull(curso.getProfesor());
+	}
+
+	@Test
+	public void queNoSePuedanAgregarMasDeDiezPersonasAUnCurso() {
+		final String nombre;
+		final String apellido;
+		final Integer dni;
+		final String email;
+		final String telefono;
+		final LocalDate fechaNacimiento;
+		Integer cantidadDeAlumnosEsperados;
+		final TipoCurso tipoCurso;
+		Alumno alumno;
+		Curso curso;
+
+		nombre = "Sebastian";
+		apellido = "Villa";
+		dni = 40402722;
+		email = "ElAlumnoVilla@gmail.com";
+		telefono = "+541128341522";
+		fechaNacimiento = LocalDate.of(1996, 5, 19);
+
+		cantidadDeAlumnosEsperados = 10;
+		tipoCurso = TipoCurso.HTML;
+
+		curso = new Curso(tipoCurso);
+
+		// Intenta agregar 11 alumnos
+		for (int i = 0; i < 11; i++) {
+			alumno = new Alumno(i, i, i, i, i, fechaNacimiento);
+			curso.añadirAlumnos(alumno, tipoCurso);
+		}
+
+		// comprueba que el ultimo alumno no se agrega xq no hay mas tamaño
+		assertEquals(cantidadDeAlumnosEsperados, curso.cantidadAlumnos());
 	}
 
 }
