83423db61d5eb1fefd410be69deb57cea8cf18b2
“Ignacio <nachorf78@gmail.com>
2023-05-12 14:36:50 -0300

    Atributos clase empresa

--- src/ar/edu/unlam/dominio/EmpresaDeCursos/Empresa.java
+++ src/ar/edu/unlam/dominio/EmpresaDeCursos/Empresa.java
@@ -1,5 +1,33 @@
 package ar.edu.unlam.dominio.EmpresaDeCursos;
 
+import java.util.ArrayList;
+
 public class Empresa {
 
+	ArrayList<Curso> cursos;
+
+	public Empresa(ArrayList<Curso> cursos) {
+		this.cursos = cursos;
+	}
+
+	public ArrayList<Curso> getCurso() {
+		return cursos;
+	}
+
+	public void setCurso(ArrayList<Curso> cursos) {
+		this.cursos = cursos;
+	}
+	
+	
+	public void agregarCurso(Curso curso) {
+		
+		if (curso.getPrecio()!=null && curso.getClases() != null && curso.getProfesor()!=null) {
+			cursos.add(curso);	
+		}
+		
+		
+	}
+	
+	
+	
 }
